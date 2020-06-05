31218fd9625eb4cc2670fbfddb520f569b016533
Gerolf Ziegenhain <g@ziegenhain.com>
2020-06-06 00:51:14 +0200

    wip workflow

--- bin/fzk_workflow.sh
+++ bin/fzk_workflow.sh
@@ -21,9 +21,13 @@ echo split      = 4.  split map data into tiles
 find work/$region/*.osm.gz ||./mt.pl split $region
 
 echo build      = 5.  build map files (img, mdx, tdb)
-./mt.pl build $region
+find work/${region}_$lang/ -name *.img ||./mt.pl build $region 
 
 echo gmap       = 6.  create gmap file (for BaseCamp OS X, Windows)
+find work/${region}_$lang/ -name *.gmap ||./mt.pl gmap $region
+
 echo nsis       = 6.  create nsis installer (full installer for Windows)
+
+
 echo gmapsupp   = 6.  create gmapsupp image (for GPS receiver)
 echo imagedir   = 6.  create image directory (e.g. for QLandkarte)
