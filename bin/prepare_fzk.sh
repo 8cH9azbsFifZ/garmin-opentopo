c0794fe2805d86df4540d9d9016a940e8fe49141
Gerolf Ziegenhain <g@ziegenhain.com>
2020-06-05 15:59:45 +0200

    fresh

--- bin/prepare_fzk.sh
+++ bin/prepare_fzk.sh
@@ -4,5 +4,7 @@ test -d ||mkdir -p /garmin/fzk
 #cp -rv /opt/fzk/Freizeitkarte-Entwicklung/* /garmin/fzk
 rsync -avP /opt/fzk/Freizeitkarte-Entwicklung/. /garmin/fzk/.
 
+cd /garmin/fzk
+
 /garmin/fzk/mt.pl bootstrap 
 
