5dec0c82128ae7bb95142ba1477b37babb5d751e
Gerolf Ziegenhain <g@ziegenhain.com>
2020-06-09 22:54:43 +0200

    split tiles and render

---
+++ bin/render_map.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#FIXME
+cd /opt/lib/fzk-mde-garmin-develop/Freizeitkarte-Entwicklung/
+
+
+
+# Prepare Typ Files
+echo "create typfiles if neccessary"
+find work/typfiles/$lang/freizeit.TYP ||./mt.pl alltypfiles
+
+
+
+# Remder the map
+# --typfile=freizeit.TYP --style=fzk
+opts="--typfile=freizeit.TYP --style=fzk" #default
+echo "build      = 5.  build map files (img, mdx, tdb)"
+./mt.pl $opts build $region 
+
+echo "gmap       = 6.  create gmap file (for BaseCamp OS X, Windows)"
+./mt.pl $opts gmap $region
+
+#echo nsis       = 6.  create nsis installer (full installer for Windows)
+# FIXME: UTF8 Problem
+
+echo "gmapsupp   = 6.  create gmapsupp image (for GPS receiver)"
+find work/${region}_$lang/$region.gmap ||./mt.pl $opts gmapsupp $region
+rsync -avP work/${region}_$lang/$region.gmap install/$region.gmap
+
+#echo imagedir   = 6.  create image directory (e.g. for QLandkarte)
